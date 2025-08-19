03a6976f13f396194c6314eb4a6646fec2958586
Chetanya Chonkar <chonkarchetanya@gmail.com>
2025-08-19 19:33:34 +0530

    Adding another new file

---
+++ index.js
@@ -0,0 +1,38 @@
+//What is Git?
+// Free & Open Source Version Control System 
+
+
+//Using Git
+// . Command Line (Most Popular)
+// . IDE /Code Editors (like VSCode)
+// . Graphical User Interface (like GitKraken)
+
+
+//Configuring Git
+
+// . git config --global user.name "My Name"
+// . git config --global user.email "someone@email.com"
+
+
+// Basic Commands 
+
+// . Clone - Cloning a repository on our local machine
+// . git clone <-some link->
+
+// . status - displays the state of the code
+// . git status
+
+
+// . add - adds new or unchanged files in your working directory to the Git staging area.
+// . git add <- file name ->
+
+
+// . commit - it is the record of change
+// . git commit -m "some message"
+
+
+// . push - upload local repo content to remote repo
+//. git push origin main
+
+
+
